25fe7e3ea64102dc337d73f3491d5e1d94d62ce2
s1140536-wq <s1140536@dmhs.kh.edu.tw>
2025-10-17 16:20:11 +0800

    Add files via upload

---
+++ HW2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 88;
+    double hight = 159.0;
+    std::string name = "Sharon";
+    bool is_pass = true;
+    std::cout << " 姓名: " << name << "身高: "<< hight << std::endl;
+    std::cout << " 年齡: " << age <<" 是否及格 "<< is_pass << std::endl;
+
+    return 0;
+}
